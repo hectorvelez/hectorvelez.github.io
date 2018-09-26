3989de55d72956952d8d4a8b34e03203e2cdf3c6
hectorvelez <37880194+hectorvelez@users.noreply.github.com>
2018-09-26 16:56:22 -0500

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,12 @@
+(function(d){
+  var l = d.getElementById("foxyform_embed_link_332332");
+      i = d.createElement('IFRAME');
+  i.src = "http://es.foxyform.com/form.php?id=332332&sec_hash=e5932eaa044";
+  i.width = "350px";
+  i.height = "446px";
+  i.frameborder = 0;
+  i.setAttribute('style', 'border: none;');
+  i.setAttribute('frameborder', '0');
+  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+  l.parentNode.insertBefore(i,l);
+})(document);
